1f29083c6b8b37ed08df2242da579f79ce97ddca
Mariam J. Hussein <Mhussein@wisefdn.org>
2026-07-04 10:55:53 -0400

    Add Student Entity

---
+++ src/main/java/com/student/demo/entity/Student.java
@@ -0,0 +1,23 @@
+package com.student.demo.entity;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private Integer age;
+}
