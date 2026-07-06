c92fe1c78cfb448d50475601deac1fa9fcf89cfb
Mariam J. Hussein <Mhussein@wisefdn.org>
2026-07-06 09:14:58 -0400

    create Student repository

---
+++ src/main/java/com/student/demo/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.student.demo.repository;
+
+import com.student.demo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
